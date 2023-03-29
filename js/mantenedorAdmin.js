f255bfc2cad6150c6232d2aeec6fdbb6460eeccb
Miguel Rondanelli <miguel.rondanelli.inf@gmail.com>
2023-03-29 19:25:48 -0300

    arreglo y eliminacion de algunas console.log y comentarios basura

--- js/mantenedorAdmin.js
+++ js/mantenedorAdmin.js
@@ -39,7 +39,7 @@ function Eventos(){
         rellenarModal(seleccionado);
 
         }
-    })
+    });
     //Envio de informacion para modificar
     btnGuardar.addEventListener('click', async ()=>{
 
@@ -59,8 +59,8 @@ function Eventos(){
             alert('Registro modificado exitosamente');
         }
 
-    })
-
+    });
+    
 
 }
 
@@ -68,44 +68,9 @@ Eventos();
 
 /*
 
-//Variables globales
-
-const btnAgregar = document.querySelector('#agregarP');
-const mantenedor1 = new Mantenedor('https://slifer.bsite.net/td-producto');
-const producto1 = new Producto(0,'Lampara de escritorio', 15000,'/img/lamp.jpg',15,'Lampara','Lampara pequeña de escritorio de alumnio',40,6);
-
-    //variables mini Mantenedor
-        //Eliminar
-        const inputBorrar = document.querySelector('#inputBorrar');
-        const btnBorrar = document.querySelector('#btnBorrar');
-        //modificar
-        const inputModId = document.querySelector('#inputMod-id');
-        const inputModNombre = document.querySelector('#inputMod-nombre');
-        const inputModDes = document.querySelector('#inputMod-des');
-        const inputModPrec = document.querySelector('#inputMod-prec');
-        const inputModCat = document.querySelector('#inputMod-cat');
-        const inputModStock = document.querySelector('#inputMod-stock');
-        const inputModImg = document.querySelector('#inputMod-img');
-        const inputModTag = document.querySelector('#inputMod-tag');
-        const btnMod = document.querySelector('#btnMod');
-
 
 //Eventos
 function Eventos(){
-
-    document.addEventListener('DOMContentLoaded', async ()=>{
-        
-        let ProductosCachureando;
-        let resultado;
-        console.log('Nuestros Productos');
-
-        ProductosCachureando = await mantenedor1.producto();
-        
-        resultado = ProductosCachureando.filter(element =>  element.idSucursal == 6);
-
-        console.log(resultado);
-
-    })
 
     btnAgregar.addEventListener('click', ()=>{
         
@@ -120,31 +85,9 @@ function Eventos(){
 
     });
 
-    btnMod.addEventListener('click', ()=>{
 
-        let obj = {
-
-            "id":inputModId.value,
-            "nombre": inputModNombre.value ,
-            "precio": inputModPrec.value,
-            "link": inputModImg.value,
-            "stock": inputModStock.value,
-            "etiqueta": inputModTag.value ,
-            "descripcion":inputModDes.value ,
-            "idCategoria": inputModCat.value,
-            "idSucursal": 6
-        }
-
-        mantenedor1.modificarProducto(obj)
-
-    });
 }
-
-Eventos();
-
-
-//
-//mantenedor1.producto();*/
+*/
 
 
 //Funciones
@@ -172,7 +115,6 @@ function rellenarModal(seleccionado){
     inputDescripcion.value = descripcionTabla;
     inputIdCategoria.value = idCategoriaTabla;
     inputIdSucursal.value = idSucursalTabla;
-    console.log(idTabla);
 
 }
 
